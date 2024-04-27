cc34e8153b0330049deee78e813a6380b0d98e0e
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 22:47:55 +0200

    Create navbar.php

---
+++ 2022/sections/navbar.php
@@ -0,0 +1,21 @@
+<table border="0" width="100%" bgcolor="#0000CC">
+    <tbody><tr>
+        <td rowspan="2" width="10%">
+        <a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/"><img src="https://web.archive.org/web/20220830122701im_/http://www.msnhomepage.altervista.org/site_l.gif"></a>
+        </td> 
+        <td colspan="3" align="right">
+        <font color="white">Sono le <?php echo  date('H:m \d\e\l d/m/y'); ?> </font>
+        </td>
+        </tr><tr>
+          <td width="30%" bgcolor="#3399FF" align="center">
+             <a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/"><font color="white">Home</font></a>
+        </td> 
+        <td width="30%" bgcolor="#3399FF" align="center">
+            <font color="white"><a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/forum">Forum</a></font>
+        </td>
+        <td width="30%" bgcolor="#3399FF" align="center">
+            <font color="white">Contatti</font>
+        </td>
+        </tr>
+    <tr>
+</tr></tbody></table>
