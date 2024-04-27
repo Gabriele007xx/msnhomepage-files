1f8a40b8cf7e09483edd594a71a7b762ef3990ac
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 22:57:07 +0200

    Create random.php

---
+++ 2022/sections/random.php
@@ -0,0 +1,8 @@
+<table border="0" height="100%" align="right">
+        <tbody><tr>
+        	<td align="center">
+            <b>Un libro a caso</b>
+            <hr>
+            Concetti e fondamenti di Java 2<br><img src="https://web.archive.org/web/20220830122701im_/http://msnhomepage.altervista.org/libro/copertine/91.jpg"><br><a href="https://web.archive.org/web/20220830122701/https://archive.org/details/concetti-e-fondamenti-di-java-2">Download 1</a>			</td>
+		</tr>
+</tbody></table>
