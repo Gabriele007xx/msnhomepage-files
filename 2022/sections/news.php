f25ab4d1aa692eff29462c40944df20b5ce1001d
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 22:55:44 +0200

    Create news.php

---
+++ 2022/sections/news.php
@@ -0,0 +1,38 @@
+<table border="0" height="100%" align="center">
+        <tbody><tr>
+        	<td>
+        		<table border="3" bordercolor="#229fce">
+<tbody><tr>
+<td bgcolor="#0ed4f0"><img src="https://web.archive.org/web/20220830122701im_/http://www.msnhomepage.altervista.org/collezione/scudetto/img/new4.gif">La sezione ClashOfStats è aperta. Visitala <a href="clashstats/">qui.</a> Ignorate l'avviso delle war!<img src="https://web.archive.org/web/20220830122701im_/http://www.msnhomepage.altervista.org/collezione/scudetto/img/new4.gif"></td>
+</tr>
+</tbody></table><br><img src="https://web.archive.org/web/20220830122701im_/http://msnhomepage.altervista.org/copertine/File_rari_sezione_aperta.png" align="right" border="0" height="107" width="150"><font color="#000000" face="Arial" size="1">16-07-2022<br>
+<font helvetica="" color="#cc3333" face="Arial," size="2"> <a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/notizie/viewn.php?id=4"><b>La Sezione File rari è aperta<br>
+</b></a></font><font color="#000000" face="Arial" size="1">di <a href="https://web.archive.org/web/20220830122701/https://web.archive.org/web/19990202125520/mailto:maister@iol.it">Gabriele</a></font></font>
+<p><font color="#000000" face="Arial" size="1"><font color="#000000" face="Arial" size="1"><font color="#000000" face="Arial" size="2">Una nuova sezione aperta!
+
+</font>
+</font></font></p>
+
+
+<hr align="left" size="1"><img src="https://web.archive.org/web/20220830122701im_/http://msnhomepage.altervista.org/copertine/mpolveroso_.png" align="right" border="0" height="107" width="150"><font color="#000000" face="Arial" size="1">07-07-2022<br>
+<font helvetica="" color="#cc3333" face="Arial," size="2"> <a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/notizie/viewn.php?id=3"><b>L'Archivio Polveroso di Gabriele è aperto!<br>
+</b></a></font><font color="#000000" face="Arial" size="1">di <a href="https://web.archive.org/web/20220830122701/https://web.archive.org/web/19990202125520/mailto:maister@iol.it">Gabriele</a></font></font>
+<p><font color="#000000" face="Arial" size="1"><font color="#000000" face="Arial" size="1"><font color="#000000" face="Arial" size="2">Da oggi è disponibile online l'Archivio Polveroso di Gabriele! E' possibile rovistarlo per trovare qualcosa di utile.
+
+
+</font>
+</font></font></p>
+
+
+<hr align="left" size="1"><img src="https://web.archive.org/web/20220830122701im_/https://web.archive.org/web/20000602161638im_/http://www.apogeonline.com/gif/news.gif" align="right" border="0" height="107" width="150"><font color="#000000" face="Arial" size="1">16-04-2022<br>
+<font helvetica="" color="#cc3333" face="Arial," size="2"> <a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/notizie/viewn.php?id=1"><b>msnhomepage 4.0 è qui!<br>
+</b></a></font><font color="#000000" face="Arial" size="1">di <a href="https://web.archive.org/web/20220830122701/https://web.archive.org/web/19990202125520/mailto:maister@iol.it">Gabriele</a></font></font>
+<p><font color="#000000" face="Arial" size="1"><font color="#000000" face="Arial" size="1"><font color="#000000" face="Arial" size="2">Dopo vario tempo ecco qui msnhomepage 4.0. Questa volta, il sito è completamente originale!
+
+</font>
+</font></font></p>
+
+
+<hr align="left" size="1">        	</td>
+        </tr><tr>
+        </tr></tbody></table>
