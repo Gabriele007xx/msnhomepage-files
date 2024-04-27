87e454c7a2b475034d4e111fd90d09c0f54224f4
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 22:52:44 +0200

    Create menu.php

---
+++ 2022/sections/menu.php
@@ -0,0 +1,25 @@
+<table border="0" align="left" width="100%" bgcolor="#000099">
+		<tbody><tr>
+        	<td>
+            <font color="white">Menu'</font>
+			</td>
+		</tr>
+        <tr>
+        	<td bgcolor="#99ffff">
+            		<form action="https://web.archive.org/web/20220830122701/https://www.google.com/search" method="get">
+    				<input type="text" name="q">
+    				<input type="submit" value="Cerca su Google" align="center">
+					</form>
+					<hr>
+                    <p align="center">News</p>
+                    <hr>
+                    <p align="center"><a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/collezione/scudetto">Scudetti</a></p>
+                    <hr>
+                    <p align="center"><a href="https://web.archive.org/web/20220830122701/http://www.msnhomepage.altervista.org/libro/list.php">Lista dei libri</a></p>
+                    <hr>
+                    <p>Ultimi libri aggiunti</p>
+                    <a href="https://web.archive.org/web/20220830122701/http://msnhomepage.altervista.org/libro/view.php?id=18">Reti neurali artificiali</a><br><a href="https://web.archive.org/web/20220830122701/http://msnhomepage.altervista.org/libro/view.php?id=17">Concetti e fondamenti di Java 2</a><br><a href="https://web.archive.org/web/20220830122701/http://msnhomepage.altervista.org/libro/view.php?id=16">PHP 5 Pocket</a><br><a href="https://web.archive.org/web/20220830122701/http://msnhomepage.altervista.org/libro/view.php?id=15">Imparare a programmare in Java con Minecraft</a><br><a href="https://web.archive.org/web/20220830122701/http://msnhomepage.altervista.org/libro/view.php?id=14">iPhone Programming</a><br><a href="https://web.archive.org/web/20220830122701/http://msnhomepage.altervista.org/libro/view.php?id=13">C - Corso completo di programmazione</a><br>                    
+			</td>
+		</tr>
+          	
+</tbody></table>
