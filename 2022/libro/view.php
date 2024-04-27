e09097da01ea128b1905261845b75c0273daf720
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 23:01:49 +0200

    Create view.php

---
+++ 2022/libro/view.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Home - msn</title>
+  </head>
+  <body>
+<?php 
+include "./sections/navbar.php";
+?>
+<table border="0" width="100%">
+  <tr>
+    <td> <?php include "./sections/menu.php"; ?></td>
+    <td> 
+    // TO DO: restore code
+    </td>
+    <td> <?php include "./sections/random.php"; ?></td>
+</tr>
+</table>
+    <table border="0" width="100%">
+      <hr>
+      Copyright MSNHOMEPAGE 2017 - <?php date("Y"); ?>
+    </table>
+</body>
+</html>
