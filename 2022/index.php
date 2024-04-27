83dbb1e46d6b6a7b86e259703194516b29a2cd46
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 22:56:43 +0200

    Update index.php

--- 2022/index.php
+++ 2022/index.php
@@ -9,8 +9,8 @@ include "./sections/navbar.php";
 <table border="0" width="100%">
   <tr>
     <td> <?php include "./sections/menu.php"; ?></td>
-    <td> <?php include "./sections/menu.php"; ?></td>
-    <td> <?php include "./sections/menu.php"; ?></td>
+    <td> <?php include "./sections/news.php"; ?></td>
+    <td> <?php include "./sections/random.php"; ?></td>
 </tr>
 </table>
 </body>
