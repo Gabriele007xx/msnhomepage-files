11a38a4ed932176f5c483e0c3206e1ced12a62c8
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2024-04-27 23:04:34 +0200

    Create index.php

---
+++ 2023/index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+  <title>Mario Fan Games Realm</title>
+</head>
+<body>
+<table width="100%" border="0">
+ <tbody><tr>
+ <td colspan="2">Mario Fan Games Realm</td><td>
+ </td></tr>
+ <tr>
+ <td colspan="2"><p align="center">Home | Forum </p></td>
+ </tr> <!-- Inizio home page-->
+ <tr>
+<td>
+ <table border="0" cellspacing="0" cellpadding="0">
+ <tbody><tr>
+ <td width="16" height="16"><img src="/web/20230307203936im_/http://msnhomepage.altervista.org/img/pipe_c.gif"></td>
+ <td style="background-image: url('/web/20230307203936im_/http://msnhomepage.altervista.org/img/pipe.png');" height="16"></td>
+ <td width="16" height="16"><img src="/web/20230307203936im_/http://msnhomepage.altervista.org/img/pipe_1.png"></td>
+ </tr>
+ <tr>
+ <td width="16" style="background-image: url('/web/20230307203936im_/http://msnhomepage.altervista.org/img/pipe_2.png');"></td> 
+ <td colspan="2"><a href="https://web.archive.org/web/20230307203936/http://www.msnhomepage.altervista.org/">Home</a><br>
+ <a href="https://web.archive.org/web/20230307203936/http://www.msnhomepage.altervista.org/discussione/">Forum</a>
+ </td>
+ </tr>
+ </tbody></table>
+ </td>
+ <td></td>
+ </tr> </tbody></table>
+
+
+
+
+</body></html>
