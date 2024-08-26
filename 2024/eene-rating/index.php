ee1e4b9d4da9abbfc3a16e20b2cbce4bd84753c3
Gabriele Lo Re <gabrolr70@gmail.com>
2024-08-26 17:11:12 +0200

    work in progress pagination

--- 2024/eene-rating/index.php
+++ 2024/eene-rating/index.php
@@ -10,18 +10,42 @@
 <body>
 <h1>EEnE episode rating</h1>
 <?php 
-$query = "SELECT * FROM eene_ep";
+$resultsPerPage=7;
+if(!isset($_GET["offset"]))
+{
+	$offset = 0;
+}
+$query = "SELECT * FROM eene_ep LIMIT " . $resultsPerPage . " OFFSET " . $offset*$resultsPerPage;
 $result = mysqli_query($con, $query);
-
+$numPages = floor(mysqli_num_rows($result) / $resultsPerPage);
+if($numPages<1)
+{
+	$numPages=1;
+}
 $rows=mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
 foreach ($rows as $row) {
 $query2 = "SELECT rating,number FROM eene_ep_rating WHERE id=" . $row["id"];
 $grab= mysqli_query($con, $query2);
+if(mysqli_num_rows($grab)==0)
+{
+$valueRating="N/A";
+$epNum="1";
+}
+else
+{
 $rating=mysqli_fetch_assoc($grab);
-echo "<div class=\"episode\">
-<img src=\"./thumb/1.png\" width=10% height=10%/>
+$valueRating=$rating["rating"] / $rating["number"];
+$epNum=$row["id"];
+}
+$class="episode1";
+if($epNum%2==0)
+{
+$class="episode2";
+}
+echo "<div class=\"" . $class .  "\">
+<img src=\"./thumb/" . $epNum . ".png\" width=10% height=10%/>
 <span>" . $row["title"] . "</span>
 <p>" . $row["description"] ."</p>
 <form action=\"index.php\" method=POST>
@@ -32,17 +56,25 @@ echo "<div class=\"episode\">
 <option value=4>4</option>
 <option value=5>5</option>
 </select>
-<input type=\"hidden\" name=\"sent\" value=" . $row["id"] . ">
+<input type=\"hidden\" name=\"sent\" value=" . $epNum . ">
 <input type=\"submit\" value=\"Rate\">
 </form>" .
-$rating["rating"] / $rating["number"]
+$valueRating
 .
 "<img src=\"./res/star.png\" height=16px width=16px/>
 </div>";
 }
-
 mysqli_close($con);
 ?>
-
+<div class="links">
+<?php
+$html = "";
+for($i=0;$i<$numPages;$i++)
+{
+	$html = $html . "<a href=\"./index.php?offset=" . $i . "\">". $i+1 ."</a>";
+}
+echo $html;
+?>
+</div>
 </body>
 </html>
